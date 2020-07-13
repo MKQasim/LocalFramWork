8c7f6b6c2a2844dbb93bacb1204b48c85892d792
Kam Qasim <kamqasim1@gmail.com>
2020-07-13 17:55:35 +0400

    Finished Our LocolFrameWork

---
+++ LocalFramWork/LocalFramWork/LocalFramWork.h
@@ -0,0 +1,19 @@
+//
+//  LocalFramWork.h
+//  LocalFramWork
+//
+//  Created by muhammad on 7/13/20.
+//  Copyright © 2020 Insurance Authority. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LocalFramWork.
+FOUNDATION_EXPORT double LocalFramWorkVersionNumber;
+
+//! Project version string for LocalFramWork.
+FOUNDATION_EXPORT const unsigned char LocalFramWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocalFramWork/PublicHeader.h>
+
+
---
+++ LocalFramWork/LocalFramWork/Services.swift
@@ -0,0 +1,20 @@
+//
+//  Services.swift
+//  LocalFramWork
+//
+//  Created by muhammad on 7/13/20.
+//  Copyright © 2020 Insurance Authority. All rights reserved.
+//
+
+import Foundation
+
+public class Services {
+	
+	
+	private init(){}
+	
+	public static func getPodVersion()->String{
+		return "getPodVersion  0.0.1"
+	}
+	
+}
